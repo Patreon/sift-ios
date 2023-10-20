8ce598d4a595e55c524521f8342f3da15d681c31
Aidan Dysart <aidan@patreon.com>
2023-10-19 17:16:31 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,15 +1,15 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "sift-ios",
+    name: "Sift",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.-
         .library(
-            name: "sift-ios",
-            targets: ["Sift", "sift-ios"]),
+            name: "Sift",
+            targets: ["Sift"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,16 +19,11 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "sift-ios",
-            dependencies: ["Sift"]),
-        .target(
             name: "Sift",
-            dependencies: [],
-            path: "Sift",
-            exclude: ["Info.plist", "README.md"],
+            exclude: ["Info.plist"],
             publicHeadersPath: "."),
         .testTarget(
-            name: "sift-iosTests",
-            dependencies: ["sift-ios"]),
+            name: "SiftTests",
+            dependencies: ["Sift"]),
     ]
 )
